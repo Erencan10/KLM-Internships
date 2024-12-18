d19020cef56fafc3f9ad74784e2aed99e50f0497
Erencan10 <131658679+Erencan10@users.noreply.github.com>
2024-12-18 14:12:21 +0100

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,5 @@
+<footer>
+    <p>&copy; 2024 KLM Internship Market. All Rights Reserved.</p>
+</footer>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <!-- <link rel="stylesheet" href="assets/css/style.css"> -->
+    <style>
+        .data {
+            background: url(images/logo_klm.png) fixed;
+        }
+        
+    </style>
+</head>
+<body>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>KLM Internship Market</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <div class="data">
+        <?php 
+            include "header.php";
+        ?>
+   
+<main style="height:100vh;">
+    <h1 style="color:white">Welcome on board to our internship market!</h1>
+    <p style="color:white">Your journey starts here. Explore opportunities, innovation, and more!</p>
+    <a href="pages/safety.php" class="btn">Let's Get Started!</a>
+</main>
+<footer>
+        <p>&copy; 2024 KLM Internship Market. All Rights Reserved.</p>
+    </footer>
+    </div>
+</body>
+</html>
+
+
